e4b3b2d4ee45ca00b37058fc6f74550e936bd5ff
Christian Sherland <christiansherland@gmail.com>
2019-12-03 13:32:17 -0500

    working day 2 solution

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -7,12 +7,14 @@ fn main() {
                                 .map(|l| l.parse().unwrap())
                                 .collect();
 
-    for command in commands.chunks(4) {
+    for command in commands.clone().chunks(4) {
         match command {
-            [1, x, y, pos] => commands[*pos as usize] = *x + *y,
-            [2, x, y, pos] => commands[*pos as usize] = *x * *y,
+            [1, x, y, pos] => commands[*pos as usize] = commands[*x as usize] + commands[*y as usize],
+            [2, x, y, pos] => commands[*pos as usize] = commands[*x as usize] * commands[*y as usize],
             [99, _, _, _] => break,
             _ => panic!("Unrecognized command")
         }
     }
+
+    println!("{:?}", commands)
 }
