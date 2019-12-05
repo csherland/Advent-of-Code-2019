ecf9403a9423aafdef9edf2ad111102def4d0780
Christian Sherland <christiansherland@gmail.com>
2019-12-05 10:05:08 -0500

    parsing inputs

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,35 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::str::FromStr;
+use std::num::ParseIntError;
+
+#[derive(Debug, PartialEq)]
+struct Edge {
+  direction: String,
+  distance: u32
+}
+
+impl FromStr for Edge {
+  type Err = ParseIntError;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    let direction = String::from(s);
+    let distance : u32 = 5;
+
+    Ok(Edge { direction, distance })
+  }
+}
+
 fn main() {
-    println!("Hello, world!");
+  let fp = File::open("./input.txt")
+                .expect("Could not find file");
+
+  let file = BufReader::new(&fp);
+
+  let wires : Vec<Vec<Edge>> = file.lines()
+                  .map(|l| l.expect("Could not read line").split(",").map(|s| s.parse::<Edge>().unwrap()).collect())
+                  .collect();
+
+  println!("{:?}", wires);
 }
