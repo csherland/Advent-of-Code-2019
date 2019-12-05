010655598209695efb15e19889931554c89ff8dc
Christian Sherland <christiansherland@gmail.com>
2019-12-05 10:43:51 -0500

    figured out how to parse edges... lol

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,7 +2,8 @@ use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::str::FromStr;
-use std::num::ParseIntError;
+use std::error;
+use std::fmt;
 
 #[derive(Debug, PartialEq)]
 struct Edge {
@@ -10,12 +11,29 @@ struct Edge {
   distance: u32
 }
 
+#[derive(Debug, Clone)]
+struct EdgeParseError;
+
+impl fmt::Display for EdgeParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Failed to parse edge")
+    }
+}
+
+// This is important for other errors to wrap this one.
+impl error::Error for EdgeParseError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        // Generic error, underlying cause isn't tracked.
+        None
+    }
+}
+
 impl FromStr for Edge {
-  type Err = ParseIntError;
+  type Err = EdgeParseError;
 
   fn from_str(s: &str) -> Result<Self, Self::Err> {
-    let direction = String::from(s);
-    let distance : u32 = 5;
+    let direction = String::from(s.get(..1).ok_or(EdgeParseError)?);
+    let distance : u32 = s[1..].parse::<u32>().or(Err(EdgeParseError))?;
 
     Ok(Edge { direction, distance })
   }
@@ -30,6 +48,6 @@ fn main() {
   let wires : Vec<Vec<Edge>> = file.lines()
                   .map(|l| l.expect("Could not read line").split(",").map(|s| s.parse::<Edge>().unwrap()).collect())
                   .collect();
-
+  
   println!("{:?}", wires);
 }
