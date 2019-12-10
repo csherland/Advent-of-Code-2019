d3f3e9239f2707ca308db2f87351fae7d7c41e06
Christian Sherland <christiansherland@gmail.com>
2019-12-10 10:55:22 -0500

    working on day 5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,44 +1,61 @@
 use std::fs;
 
-fn main() {
-    let mut commands:Vec<u32> = fs::read_to_string("input.txt")
-                                .unwrap()
-                                .split(",")
-                                .map(|l| l.parse().unwrap())
-                                .collect();
-
-    for i in 0..commands.len() {
-        
-        // Identify command modes and op code
-        let command = parseCommand(commands[i]);
-
-        match operation {
-            1 => commands[*pos as usize] = commands[*x as usize] + commands[*y as usize],
-            2 => commands[*pos as usize] = commands[*x as usize] * commands[*y as usize],
-            3 => commands[*pos as usize] = input,
-            4 => input = commands[*pos as usize],
-            99 => break,
-            _ => panic!("Uknown command")
-        }
-
-        // Figure out how to do this properly
-        for j in 0..command.len() {
-            i.next();
-        }
-    }
-
-    println!("{:?}", commands)
+struct Instruction {
+    op_code: i32,
+    step: i32,
+    modes: Vec<i32>
 }
 
-fn parseCommand(command: u32) -> Vec<u32> {
+fn parseInstruction(instruction: i32) -> Instruction {
     let mut res = vec![];
 
-    let mut i = command;
+    let mut i = instruction;
     while i > 0 {
         res.push(i % 10);
         i /= 10;
     }
-    res.reverse();
 
-    res
+    let op_code = 0;
+    let modes = vec![];
+
+    let mut step = 0;
+    match op_code {
+        1 | 2 => step = 4,
+        3 | 4 => step = 2,
+        99 => (),
+        _ => panic!("Unrecognized operation code")
+    }
+
+    Instruction { op_code, step, modes }
+}
+
+fn main() {
+    let mut commands:Vec<i32> = fs::read_to_string("input.txt")
+                                .unwrap()
+                                .split(",")
+                                .map(|l| l.parse().unwrap())
+                                .collect();
+    let mut input = 1;
+
+    let mut i = 0;
+    while i < commands.len() {
+        // Identify parameter modes and op code
+        let instruction = parseInstruction(commands[x]);
+
+        let mut x:i32 = 0;
+        let mut y:i32 = 0;
+        let mut pos:usize = 0;
+
+        match instruction.op_code {
+            1 => commands[pos as usize] = commands[x as usize] + commands[y as usize],
+            2 => commands[pos as usize] = commands[x as usize] * commands[y as usize],
+            3 => commands[pos as usize] = input,
+            4 => println!("{}", commands[pos as usize]), // Output command
+            99 => break,
+            _ => panic!("Uknown command")
+        }
+
+        i += instruction.step as usize;
+    }
+
 }
