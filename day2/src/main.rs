a6edf66dad00366c779eafc21079f39842cb5ab6
Christian Sherland <christiansherland@gmail.com>
2019-12-11 16:57:39 -0500

    working on day 2 solution pt 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -7,14 +7,28 @@ fn main() {
                                 .map(|l| l.parse().unwrap())
                                 .collect();
 
-    for command in commands.clone().chunks(4) {
-        match command {
-            [1, x, y, pos] => commands[*pos as usize] = commands[*x as usize] + commands[*y as usize],
-            [2, x, y, pos] => commands[*pos as usize] = commands[*x as usize] * commands[*y as usize],
-            [99, _, _, _] => break,
-            _ => panic!("Unrecognized command")
+    for x in 0..100 {
+        for y in 0..100 {
+            let mut commands_copy = commands.clone();
+            commands_copy[1] = x;
+            commands_copy[2] = y;
+
+            for command in commands_copy.clone().chunks(4) {
+                if x as usize > commands.len() || y as usize > commands.len() || command[3] as usize > commands.len() {
+                    break;
+                }
+                match command {
+                    [1, x, y, pos] => commands[*pos as usize] = commands[*x as usize] + commands[*y as usize],
+                    [2, x, y, pos] => commands[*pos as usize] = commands[*x as usize] * commands[*y as usize],
+                    [99, _, _, _] => break,
+                    _ => ()
+                }
+            }
+
+            if commands_copy[0] == 19_690_720 {
+                println!("Soulution found, x: {}, y: {}", x, y);
+                break;
+            }
         }
     }
-
-    println!("{:?}", commands)
 }
