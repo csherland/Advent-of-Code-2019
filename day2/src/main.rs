bd8bf2f3e0e1a3b15a7467a6f76a2512636f5ef4
Christian Sherland <christiansherland@gmail.com>
2019-12-12 10:13:07 -0500

    2 star solution for day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 fn main() {
-    let mut commands:Vec<i32> = fs::read_to_string("input.txt")
+    let commands:Vec<i32> = fs::read_to_string("input.txt")
                                 .unwrap()
                                 .split(",")
                                 .map(|l| l.parse().unwrap())
@@ -13,22 +13,27 @@ fn main() {
             commands_copy[1] = x;
             commands_copy[2] = y;
 
-            for command in commands_copy.clone().chunks(4) {
-                if x as usize > commands.len() || y as usize > commands.len() || command[3] as usize > commands.len() {
-                    break;
-                }
-                match command {
-                    [1, x, y, pos] => commands[*pos as usize] = commands[*x as usize] + commands[*y as usize],
-                    [2, x, y, pos] => commands[*pos as usize] = commands[*x as usize] * commands[*y as usize],
-                    [99, _, _, _] => break,
-                    _ => ()
-                }
-            }
+            let result = run_int_code_program(commands_copy);
 
-            if commands_copy[0] == 19_690_720 {
-                println!("Soulution found, x: {}, y: {}", x, y);
-                break;
+            if result[0] == 19_690_720 {
+                panic!("Soulution found, x: {}, y: {}", x, y);
             }
         }
     }
+}
+
+fn run_int_code_program(mut program: Vec<i32>) -> Vec<i32> {
+    for command in program.clone().chunks(4) {
+        if command[1] as usize > program.len() || command[2] as usize > program.len() || command[3] as usize > program.len() {
+            break;
+        }
+        match command {
+            [1, x, y, pos] => program[*pos as usize] = program[*x as usize] + program[*y as usize],
+            [2, x, y, pos] => program[*pos as usize] = program[*x as usize] * program[*y as usize],
+            [99, _, _, _] => break,
+            _ => ()
+        }
+    }
+
+    program
 }
