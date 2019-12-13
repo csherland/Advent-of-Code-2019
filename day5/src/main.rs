5422d00141d1a99f70112b2468fda333219fa484
Christian Sherland <christiansherland@gmail.com>
2019-12-12 19:02:00 -0500

    day 5 close to working

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,8 +1,9 @@
 use std::fs;
 
+#[derive(Debug)]
 struct Instruction {
     op_code: i32,
-    step: i32,
+    step: usize,
     modes: Vec<i32>
 }
 
@@ -20,9 +21,7 @@ fn parse_instruction(mut instruction: i32) -> Instruction {
         op_code = parameters[0];
     }
 
-    let modes = vec![];
-
-    let mut step = 0;
+    let mut step: usize = 0;
     match op_code {
         1 | 2 => step = 4,
         3 | 4 => step = 2,
@@ -30,34 +29,43 @@ fn parse_instruction(mut instruction: i32) -> Instruction {
         _ => panic!("Unknown command")
     }
 
+    let mut modes: Vec<i32> = vec![0; step - 1];
+    if parameters.len() > 2 {
+        for (i, mode) in parameters[2..].iter().enumerate() {
+            modes[i] = *mode;
+        }
+    } 
+
     Instruction { op_code, step, modes }
 }
 
-fn run_intcode_program(mut program: Vec<i32>, input: i32) -> Vec<i32> {
+fn run_intcode_program(mut program: Vec<i32>, input: i32) {
     let mut i = 0;
     while i < program.len() {
         // Identify parameter modes and op code
         let instruction = parse_instruction(program[i]);
 
-        // Need to set these based on instruction
-        let mut x:i32 = 0;
-        let mut y:i32 = 0;
-        let mut pos:usize = 0;
-
-
+        let mut parameters = vec![];
+        for (j, mode) in instruction.modes.iter().enumerate() {
+            if *mode == 0 {
+                parameters.push(program[program[i + j + 1 as usize] as usize]);
+            } else {
+                parameters.push(program[i + j + 1 as usize]);
+            }
+        }
+        
+        let pos = program[i + instruction.step - 1];
         match instruction.op_code {
-            1 => program[pos] = x + y,
-            2 => program[pos] = x * y,
-            3 => program[pos] = input,
-            4 => println!("{}", program[pos]), // Output command
+            1 => program[pos as usize] = parameters[0] + parameters[1],
+            2 => program[pos as usize] = parameters[0] * parameters[1],
+            3 => program[pos as usize] = input,
+            4 => println!("{}", program[parameters[0] as usize]), // Output command
             99 => break,
             _ => panic!("Unknown command")
         }
 
-        i += instruction.step as usize;
+        i += instruction.step;
     }
-
-    program
 }
 
 fn main() {
@@ -67,7 +75,5 @@ fn main() {
                                 .map(|l| l.parse().unwrap())
                                 .collect();
 
-    let result = run_intcode_program(program, 1);
-
-    println!("Program result: {}", result[0]);
+    run_intcode_program(program, 1);
 }
