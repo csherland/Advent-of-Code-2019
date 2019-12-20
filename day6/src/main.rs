b90d11d6d1e026522e90cd5b03cdedc26ec1268a
Christian Sherland <christiansherland@gmail.com>
2019-12-20 09:40:47 -0500

    1 star solution for day 6

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,12 +1,7 @@
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
-
-#[derive(Debug)]
-struct Orbit {
-    name: String,
-    orbitals: Vec<Orbit>
-} 
+use std::collections::HashMap;
 
 fn main() {
     let fp = File::open("./input.txt")
@@ -18,25 +13,75 @@ fn main() {
                 .map(|l| l.expect("Could not read line").parse().unwrap())
                 .collect();
 
-    let mut orbits : Vec<Orbit> = vec![];
-    for orbit in map {
-        let (name, moon) = orbit.split_at(orbit.find(")").unwrap());
+    // Create the orbit list
+    let mut orbits : HashMap<String, Vec<String>> = HashMap::new();
+    for orbit in map.clone() {
+        let o: Vec<&str> = orbit.split(")").collect();//orbit.split_at(orbit.find(")").unwrap());
+        let planet = String::from(o[0]);
+        let moon = String::from(o[1]);
 
-        let child;
-        match orbits.iter().find(|&x| x.name == name) {
-            Some(o) => child = *o,
-            None => { 
-                child = Orbit { name: String::from(moon), orbitals: vec![] };
-                orbits.push(child);
+        match orbits.get(&planet) {
+            Some(o) => {
+                let mut new_moon = o.clone();
+                new_moon.push(moon);
+                orbits.insert(planet, new_moon);
+            },
+            None => {
+                orbits.insert(planet, vec![String::from(moon)]);
             }
         }
 
-        println!("{:?}", child);
-
-        // parent.orbitals.push(child);
+        match orbits.get(o[1]) {
+            Some(_) => (),
+            None => {
+                orbits.insert(String::from(o[1]), vec![]);
+            }
+        }
     }
 
-    // let center_of_mass = get_orbit(orbits, String::from("COM"));
+    let (direct, indirect) = count_orbits(orbits, "COM", 0);
 
-    // println!("{:?}", center_of_mass);
+    println!("Direct Orbits: {:?}", direct);
+    println!("Indirect Orbits: {:?}", indirect);
+    println!("Total: {:?}", direct + indirect);
 }
+
+fn count_orbits(orbits: HashMap<String, Vec<String>>, name: &str, level: u32) -> (u32, u32) {
+    let mut direct = 0;
+    let mut indirect = 0;
+
+    let orbitals;
+    match orbits.get(name) {
+        Some(o) => orbitals = o,
+        None => return (direct, indirect)
+    }
+
+    for orbital in orbitals {
+        direct += 1;
+        indirect += level;
+
+        let (d, i) = count_orbits(orbits.clone(), orbital, level + 1);
+        direct += d;
+        indirect += i;
+    }
+
+    (direct, indirect)
+}
+
+// fn get_distance(orbits: HashMap<String, Vec<String>>, parent: &str, child: &str, level: u32) -> u32 {
+//     let orbitals;
+//     match orbits.get(child) {
+//         Some(o) => orbitals = o,
+//         None => return 0
+//     }
+
+//     for orbital in orbitals {
+//         if (get_distance(orbits.clone(), parent, child, level +1) > ) {
+//         }
+//         let (d, i) = count_orbits(orbits.clone(), orbital, level + 1);
+//     }
+// }
+
+// fn common_ancestor() {
+
+// }
